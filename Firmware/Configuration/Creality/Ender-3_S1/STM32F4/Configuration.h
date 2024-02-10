f056cdaa4a41306e458c8fd07790f4ca6932f883
JJR3D <jrodriguez@qitec.net>
2024-02-10 10:05:55 +0100

    Updated STRING_CONFIG_H_AUTHOR

--- Firmware/Configuration/Creality/Ender-3_S1/STM32F4/Configuration.h
+++ Firmware/Configuration/Creality/Ender-3_S1/STM32F4/Configuration.h
@@ -63,7 +63,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(tiagofreire-pt, Ender 3 S1 F4)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(3DWork, https://3dwork.io)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
